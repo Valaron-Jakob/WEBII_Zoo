45c5a2aa7694ddb3d0b75e23d31d7e70ec07c7b6
Valaron-Jakob <52544138+Valaron-Jakob@users.noreply.github.com>
2023-04-26 15:02:14 +0200

    Solution Task 3

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,16 @@
-// a. Log current time and date to the console
-console.log(new Date())
+// // a. Log current time and date to the console
+// console.log(new Date())
 
-// b. Message popup on load
-alert('Willkommen im Zoo von Fröhlichsheim!')
+// // b. Message popup on load
+// alert(`Willkommen im Zoo von Fröhlichsheim!`)
 
-// c. A text containing a different animal name every reload
-randomAnimalName('animal-text')
+// // c. A text containing a different animal name every reload
+// const ANIMAL_NAMES = ['Lion', 'Tiger', 'Snake']
+// const RAND_ANIMAL = ANIMAL_NAMES[Math.floor(Math.random() * ANIMAL_NAMES.length)]
 
-function randomAnimalName(elementID) {
-    const ANIMAL_NAMES = ['Lion', 'Tiger', 'Elephant']
-    const RAND_ANIMAL = ANIMAL_NAMES[Math.floor(Math.random() * ANIMAL_NAMES.length)]
-    const ANIMAL_TEXT = 'The Fröhlichsheim zoo is home to a variety of different animals. ' +
-            'Today we have a ' + RAND_ANIMAL + ' in our zoo, eating a squirrel.'
-    
-    document.getElementById(elementID).innerHTML = ANIMAL_TEXT
-}
+// document.getElementById('animal-text').innerHTML = `The Fröhlichsheim zoo is home to a variety of different animals.
+//     Today we have a ${RAND_ANIMAL} in our zoo, eating a squirrel.`
 
-// d. A text used on different webpages
-animalDesc('animal-desc')
-
-function animalDesc(elementID) {
-    const ANIMAL_DESC = '<p>Here in this zoo, we feed our animals with wild living squirrels most of the time. ' +
-        'Mice and rats are also a good alternative.</p>'
-
-    document.getElementById(elementID).innerHTML = ANIMAL_DESC
-}
-
-
-
+// // d. A text usable on different webpages
+// document.getElementById('animal-desc').innerHTML = `<p>Here in this zoo, we feed our animals with wild living squirrels 
+//     most of the time. Mice and rats are also a good alternative.</p>`
