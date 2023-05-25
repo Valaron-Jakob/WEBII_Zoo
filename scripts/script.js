79e8e2a5551c0cfa05199df08a3ced14e4ff4096
Valaron-Jakob <52544138+Valaron-Jakob@users.noreply.github.com>
2023-05-25 08:39:51 +0200

    Task 5

--- scripts/script.js
+++ scripts/script.js
@@ -1,66 +1,113 @@
-// Das Array mit den fünf auswählbaren Tierbildern
-const ANIMAL_IMG = ['<img src="assets/lion.jpg" alt="animal1">', '<img src="assets/elephant.jpg" alt="animal2">', '<img src="assets/ape.jpg" alt="animal3">',
-                    '<img src="assets/oryx.jpg" alt="animal4">', '<img src="assets/giraffe.jpg" alt="animal5">']
-
-// Die Funktion, die eines der Bilder zufällig heraussucht
-// Inputs:  Die ID des Elements, in das das Bild eingefügt werden soll
-//          Das Array mit den möglichen Bildern
-function InsertRandomAnimalIMG(element, images) {
-    // RAND_INDEX ist eine zufällige Zahl zwischen 0 und der Länge des Arrays
-    const RAND_INDEX = Math.floor(Math.random() * images.length)
-    // RAND_IMG ist der String mit dem Bild an der Stelle des zufälligen Index
-    const RAND_IMG = images[RAND_INDEX]
-    // ELEMENT ist das div Element auf der Website
-    const ELEMENT = document.getElementById(element)
-
-    // Über "innerHTML" wird dann das Bild in das angegebene div eingefügt
-    ELEMENT.innerHTML = RAND_IMG
-}
-
-// Diese Funktion wird dann für jedes der drei Elemente einmal ausgeführt
-InsertRandomAnimalIMG('image1', ANIMAL_IMG)
-InsertRandomAnimalIMG('image2', ANIMAL_IMG)
-InsertRandomAnimalIMG('image3', ANIMAL_IMG)
-
-// ! Auf diese Weise erhält man drei VÖLLIG zufällige Bilder aus dem Array, kann also auch dreimal das selbe Bild bekommen !
-
-
-
-
-//--------------------------------------------------------------------------------------------------------------------------------------
-
-// Ein zusätzliches Array mit den IDs der Elemente
-const IMG_ELEMENTS = ['image1', 'image2', 'image3']
-
-// Eine ALTERNATIVE Funktion um immer drei unterschiedliche Bilder zu erhalten
-// Inputs:  Die IDs der Elemente als Array
-//          Das Array mit den möglichen Bildern
-function InsertRandAnimIMGs(elements, images) {
-    // Die Länge des Arrays mit den IDs
-    const ELEM_LENGTH = elements.length
-    // Die Länge des Arrays mit den Bildern
-    const IMG_LENGTH = images.length
-    // Der Index eines zufälligen Bildes als Startpunkt
-    const START_INDEX = RandIndex(IMG_LENGTH)
-
-    // Eine For-Schleife, die einmal alle IDs in unserem zusätzlichen Array durchläuft
-    for ( i = 0; i < ELEM_LENGTH; i++ ) {
-        // Das aktuelle Element
-        let element = document.getElementById(elements[i])
-        // Der Startindex + die Anzahl der Durchläufe
-        //  So wird bei jedem Durchlauf das jeweils nächste Bild ausgewählt
-        //  Über "% IMG_LENGTH" wird dafür gesorgt, dass nach dem letzten Bild wieder mit dem ersten angefangen wird
-        let current_index = ( START_INDEX + i ) % IMG_LENGTH
-
-        // Das aktuelle Bild wird in das aktuelle Element eingefügt
-        element.innerHTML = images[ current_index ]
+const ANIMAL_DATA = {
+    'lion': {
+        'name': '<h3>Lion</h3>',
+        'image': '<img src="assets/lion.jpg" alt="animal1">',
+        'info': [
+            '<p>Erster Text: Lorem Ipsum dolor sit amet, consecutor 1</p>',
+            '<p>Zweiter Text: Lorem Ipsum dolor sit amet, consecutor 2</p>',
+            '<p>Dritter Text: Lorem Ipsum dolor sit amet, consecutor 3</p>',
+            '<p>Vierter Text: Lorem Ipsum dolor sit amet, consecutor 4</p>'
+        ]
+    },
+    'elephant': {
+        'name': '<h3>Elefant</h3>',
+        'image': '<img src="assets/elephant.jpg" alt="animal2">',
+        'info': [
+            '<p>Erster Text: Lorem Ipsum dolor sit amet, consecutor 1</p>',
+            '<p>Zweiter Text: Lorem Ipsum dolor sit amet, consecutor 2</p>',
+            '<p>Dritter Text: Lorem Ipsum dolor sit amet, consecutor 3</p>',
+            '<p>Vierter Text: Lorem Ipsum dolor sit amet, consecutor 4</p>'
+        ]
+    },
+    'ape': {
+        'name': '<h3>Monkey</h3>',
+        'image': '<img src="assets/ape.jpg" alt="animal3">',
+        'info': [
+            '<p>Erster Text: Lorem Ipsum dolor sit amet, consecutor 1</p>',
+            '<p>Zweiter Text: Lorem Ipsum dolor sit amet, consecutor 2</p>',
+            '<p>Dritter Text: Lorem Ipsum dolor sit amet, consecutor 3</p>',
+            '<p>Vierter Text: Lorem Ipsum dolor sit amet, consecutor 4</p>'
+        ]
+    },
+    'oryx': {
+        'name': '<h3>Oryx</h3>',
+        'image': '<img src="assets/oryx.jpg" alt="animal4">',
+        'info': [
+            '<p>Erster Text: Lorem Ipsum dolor sit amet, consecutor 1</p>',
+            '<p>Zweiter Text: Lorem Ipsum dolor sit amet, consecutor 2</p>',
+            '<p>Dritter Text: Lorem Ipsum dolor sit amet, consecutor 3</p>',
+            '<p>Vierter Text: Lorem Ipsum dolor sit amet, consecutor 4</p>'
+        ]
+    },
+    'giraffe': {
+        'name': '<h3>Giraffe</h3>',
+        'image': '<img src="assets/giraffe.jpg" alt="animal5">',
+        'info': [
+            '<p>Erster Text: Lorem Ipsum dolor sit amet, consecutor 1</p>',
+            '<p>Zweiter Text: Lorem Ipsum dolor sit amet, consecutor 2</p>',
+            '<p>Dritter Text: Lorem Ipsum dolor sit amet, consecutor 3</p>',
+            '<p>Vierter Text: Lorem Ipsum dolor sit amet, consecutor 4</p>'
+        ]
     }
 }
 
-// Das Generieren einer zufälligen Zahl aus der Länge eines Arrays als Funktion
+const TABLE_ELEMENTS = ['animal-1', 'animal-2', 'animal-3']
+
+let infos = {
+    'info-1': {
+        'infos': [],
+        'index': 0
+    },
+    'info-2': {
+        'infos': [],
+        'index': 0
+    },
+    'info-3': {
+        'infos': [],
+        'index': 0
+    }
+}
+
+function GetRandAnimals(elements, animals) {
+    const ELEM_LENGTH = elements.length
+    const KEYS_LENGTH = Object.keys(animals).length
+    const START_KEY = RandIndex(KEYS_LENGTH)
+
+    let animal_ids = []
+
+    for ( i = 0; i < ELEM_LENGTH; i++ ) {
+        animal_key = ( START_KEY + i ) % KEYS_LENGTH
+        animal_ids[i] = Object.keys(animals)[ animal_key ]
+    }
+    
+    return animal_ids
+}
+
+function SetAnimalData(animal_ids, animals) {
+    for ( i = 0; i < animal_ids.length; i++ ) {
+        let element_name = document.getElementById('animal-' + (i + 1) + '-name')
+        let element_image = document.getElementById('animal-' + (i + 1) + '-image')
+        let element_info = document.getElementById('animal-' + (i + 1) + '-info')
+
+        element_name.innerHTML = animals[ animal_ids[i] ]['name']
+        element_image.innerHTML += animals[ animal_ids[i] ]['image']
+        element_info.innerHTML += animals[ animal_ids[i] ]['info'][0]
+
+        infos['info-' + (i + 1)]['infos'] = animals[ animal_ids[i] ]['info']
+    }
+}
+
 function RandIndex(array_length) {
     return Math.floor(Math.random() * array_length)
 }
 
-// Die Funktion wird dann einmal ausgeführt
-InsertRandAnimIMGs(IMG_ELEMENTS, ANIMAL_IMG)
+function ScrollInfo(i) {
+    let element = document.getElementById('animal-' + i + '-info')
+    let curr_index = infos['info-' + i]['index']
+    let infos_length = infos['info-' + i]['infos'].length
+
+    element.innerHTML = infos['info-' + i]['infos'][ (curr_index + 1) % infos_length ]
+    infos['info-' + i]['index'] = (curr_index + 1) % infos_length
+}
+
+SetAnimalData(GetRandAnimals(TABLE_ELEMENTS, ANIMAL_DATA), ANIMAL_DATA)
